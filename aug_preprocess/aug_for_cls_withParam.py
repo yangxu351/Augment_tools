727ee165ee14a01fa26557c6b50aaa66647b0786
yangxu351 <1554404832@qq.com>
2025-01-13 15:48:02 +0800

    rename folder, aug independently

--- aug_preprocess/aug_for_cls_withParam.py
+++ aug_preprocess/aug_for_cls_withParam.py
@@ -18,9 +18,10 @@ def alb_aug_cls(base_dir, split='val', method_params={}, combine=False, exec_num
     save_dir = os.path.join(base_dir , split + '_albaug_param')
     make_dir_if_not_exist(save_dir, rm=False)
 
+    all_trans = albaug.get_trans() 
     dict_trans = {}
     for method, params in method_params.items():
-        dict_trans[method] = albaug.get_trans()[method](**params)
+        dict_trans[method] = all_trans[method](**params)
 
     cat_folders = os.listdir(source_dir)
     
@@ -28,26 +29,26 @@ def alb_aug_cls(base_dir, split='val', method_params={}, combine=False, exec_num
         img_names = os.listdir(os.path.join(source_dir, cf))
         img_names.sort()
         img_list = []
-        for n in img_names[:2]:# FIXME:
+        for n in img_names:# FIXME:
             img = np.array(Image.open(os.path.join(source_dir,cf,n)))
             img_list.append(img)
 
-        c_save_dir = os.path.join(save_dir, cf)
-        make_dir_if_not_exist(c_save_dir, rm=True)
         for en in range(exec_num):
             current_time = datetime.datetime.now()
             formatted_time = current_time.strftime("%m%d-%H%M")
-            if combine:
-                aug = alb.Compose([*dict_trans.values()])
+            # if combine:
+            #     aug = alb.Compose([*dict_trans.values()])
+            #     new_img_list = aug(images=img_list)['images']
+            #     for nx, n_img in enumerate(new_img_list):
+            #         n_file = os.path.join(c_save_dir, f"{img_names[nx].split('.')[0]}_ex{en}_comb_{formatted_time}{dst_img_suffix}")
+            #         Image.fromarray(n_img).save(n_file) 
+            # else: # independently
+            for key, aug in dict_trans.items():
+                c_save_dir = os.path.join(save_dir, cf, key)
+                make_dir_if_not_exist(c_save_dir, rm=True)
                 new_img_list = aug(images=img_list)['images']
                 for nx, n_img in enumerate(new_img_list):
-                    n_file = os.path.join(c_save_dir, f"{img_names[nx].split('.')[0]}_ex{en}_comb_{formatted_time}{dst_img_suffix}")
-                    Image.fromarray(n_img).save(n_file) 
-            else: # independently
-                for key, aug in dict_trans.items():
-                    new_img_list = aug(images=img_list)['images']
-                    for nx, n_img in enumerate(new_img_list):
-                        n_file = os.path.join(c_save_dir, f"{img_names[nx].split('.')[0]}_ex{en}_{key}_{formatted_time}{dst_img_suffix}")
-                        Image.fromarray(n_img).save(n_file)        
+                    n_file = os.path.join(c_save_dir, f"{img_names[nx].split('.')[0]}_ex{en+1}_{formatted_time}{dst_img_suffix}")
+                    Image.fromarray(n_img).save(n_file)        
 
         
