481d82f707fc4d379d46158834bd4c9b8a42f8f9
yangxu351 <1554404832@qq.com>
2025-01-14 14:21:57 +0800

    2 aug lib, ImgAug, Albumentations

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from aug_preprocess.aug_for_msk import imgaug_msk, alb_aug_msk
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/MSTAR', help='classification  test')
-    # parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/COCO', help='object detection val20217')
+    # parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/MSTAR', help='classification  test')
+    parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/COCO', help='object detection val20217')
     # parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/Lunar', help='segmentation train')
-    parser.add_argument('--split', type=str, default='test', help='[test, val2017, train]')
-    parser.add_argument('--task', type=str, default='cls', help='[cls, odt, seg]')
+    parser.add_argument('--split', type=str, default='val2017', help='[test, val2017, train]')
+    parser.add_argument('--task', type=str, default='odt', help='[cls, odt, seg]')
     parser.add_argument('--img_suffix', type=str, default='.jpg', help='.png .jpg')
     parser.add_argument('--lbl_suffix', type=str, default='.txt', help='.txt')
     args = parser.parse_args()
 
     # use ImgAug
-    if args.task == 'cls':
-        imgaug_cls(args.base_dir, args.split)
+    # if args.task == 'cls':
+    #     imgaug_cls(args.base_dir, args.split)
     # elif args.task == 'odt':
     #     imgaug_odt(args.base_dir, args.split)
     # elif args.task == 'seg':
@@ -25,11 +25,11 @@ if __name__ == '__main__':
     #     print('please input task!!!')
 
     # use Albumentations
-    # if args.task == 'cls':
-    #     alb_aug_cls(args.base_dir, args.split)
-    # elif args.task == 'odt':
-    #     alb_aug_odt(args.base_dir, args.split)
-    # elif args.task == 'seg':
-    #     alb_aug_msk(args.base_dir, args.split)
-    # else:
-    #     print('please input task!!!')
+    if args.task == 'cls':
+        alb_aug_cls(args.base_dir, args.split)
+    elif args.task == 'odt':
+        alb_aug_odt(args.base_dir, args.split)
+    elif args.task == 'seg':
+        alb_aug_msk(args.base_dir, args.split)
+    else:
+        print('please input task!!!')
