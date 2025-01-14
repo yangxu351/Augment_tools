10781120036baf0aa530492f134dd37cb74ef5e9
yangxu351 <1554404832@qq.com>
2025-01-14 11:51:36 +0800

    obtain img suffix without params

--- aug_preprocess/aug_for_cls_withParam.py
+++ aug_preprocess/aug_for_cls_withParam.py
@@ -6,7 +6,7 @@ from img_albumentation import img_aug_param as albaug
 import albumentations as alb
 import datetime
 
-def alb_aug_cls(base_dir, split='val', method_params={}, combine=False, exec_num=1, dst_img_suffix='.jpg'):
+def alb_aug_cls(base_dir, split='val', method_params={}, combine=False, exec_num=1):
     '''
     method_params dict {
                         'rotate': {'limit':(-90,90)}
@@ -29,26 +29,31 @@ def alb_aug_cls(base_dir, split='val', method_params={}, combine=False, exec_num
         img_names = os.listdir(os.path.join(source_dir, cf))
         img_names.sort()
         img_list = []
-        for n in img_names:# FIXME:
-            img = np.array(Image.open(os.path.join(source_dir,cf,n)))
-            img_list.append(img)
-
         for en in range(exec_num):
             current_time = datetime.datetime.now()
             formatted_time = current_time.strftime("%m%d-%H%M")
-            # if combine:
-            #     aug = alb.Compose([*dict_trans.values()])
-            #     new_img_list = aug(images=img_list)['images']
-            #     for nx, n_img in enumerate(new_img_list):
-            #         n_file = os.path.join(c_save_dir, f"{img_names[nx].split('.')[0]}_ex{en}_comb_{formatted_time}{dst_img_suffix}")
-            #         Image.fromarray(n_img).save(n_file) 
-            # else: # independently
             for key, aug in dict_trans.items():
                 c_save_dir = os.path.join(save_dir, cf, key)
                 make_dir_if_not_exist(c_save_dir, rm=True)
-                new_img_list = aug(images=img_list)['images']
-                for nx, n_img in enumerate(new_img_list):
-                    n_file = os.path.join(c_save_dir, f"{img_names[nx].split('.')[0]}_ex{en+1}_{formatted_time}{dst_img_suffix}")
-                    Image.fromarray(n_img).save(n_file)        
+                for n in img_names[:2]:# FIXME:
+                    img = np.array(Image.open(os.path.join(source_dir,cf,n)))
+                    img_name_pref, img_name_suff = img_names.split('.')
+                    # img_list.append(img)
+                    n_img = aug(image=img)['image']
+                    n_file = os.path.join(c_save_dir, f"{img_name_pref}_ex{en+1}_{formatted_time}.{img_name_suff}")
+                    Image.fromarray(n_img).save(n_file)
+
+                # if combine:
+                #     aug = alb.Compose([*dict_trans.values()])
+                #     new_img_list = aug(images=img_list)['images']
+                #     for nx, n_img in enumerate(new_img_list):
+                #         n_file = os.path.join(c_save_dir, f"{img_names[nx].split('.')[0]}_ex{en}_comb_{formatted_time}{dst_img_suffix}")
+                #         Image.fromarray(n_img).save(n_file) 
+                # else: # independently
+            
+                # new_img_list = aug(images=img_list)['images']
+                # for nx, n_img in enumerate(new_img_list):
+                #     n_file = os.path.join(c_save_dir, f"{img_names[nx].split('.')[0]}_ex{en+1}_{formatted_time}{dst_img_suffix}")
+                #     Image.fromarray(n_img).save(n_file)        
 
         
