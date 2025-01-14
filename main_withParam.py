da2ebe0b698335fedf2183bdbe53f6c88a341c60
yangxu351 <1554404832@qq.com>
2025-01-14 14:20:29 +0800

    ignore warnings

--- main_withParam.py
+++ main_withParam.py
@@ -2,21 +2,25 @@ import argparse
 from aug_preprocess.aug_for_cls_withParam import alb_aug_cls
 from aug_preprocess.aug_for_odt_withParam import alb_aug_odt
 from aug_preprocess.aug_for_msk_withParam import alb_aug_msk
+import os 
+import warnings
 
 if __name__ == '__main__':
+    os.environ['NO_ALBUMENTATIONS_UPDATE'] = '1'
+    warnings.filterwarnings(action='ignore', category=UserWarning)
     parser = argparse.ArgumentParser()
     # parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/MSTAR', help='classification  test')
     # parser.add_argument('--split', type=str, default='test', help='[test, val2017, train]')
     # parser.add_argument('--task', type=str, default='cls', help='[cls, odt, seg]')
 
-    parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/COCO', help='object detection val20217')        
-    parser.add_argument('--split', type=str, default='val2017', help='[test, val2017, train]')
-    parser.add_argument('--task', type=str, default='odt', help='[cls, odt, seg]')
-    parser.add_argument('--lbl_format', type=str, default='coco', help='coco, voc, yolo, default')
+    # parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/COCO', help='object detection val20217')        
+    # parser.add_argument('--split', type=str, default='val2017', help='[test, val2017, train]')
+    # parser.add_argument('--task', type=str, default='odt', help='[cls, odt, seg]')
+    # parser.add_argument('--lbl_format', type=str, default='coco', help='coco, voc, yolo, default')
 
-    # parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/Lunar', help='segmentation train')
-    # parser.add_argument('--split', type=str, default='train', help='[test, val2017, train]')
-    # parser.add_argument('--task', type=str, default='seg', help='[cls, odt, seg]')
+    parser.add_argument('--base_dir', type=str, default='F:/Public_Dataset/ForAug/Lunar', help='segmentation train')
+    parser.add_argument('--split', type=str, default='train', help='[test, val2017, train]')
+    parser.add_argument('--task', type=str, default='seg', help='[cls, odt, seg]')
 
     args = parser.parse_args()
     
@@ -58,7 +62,7 @@ if __name__ == '__main__':
 
     }
     if args.task == 'cls':
-        alb_aug_cls(args.base_dir, method_params=method_params, exec_num=2)
+        alb_aug_cls(args.base_dir, args.split, method_params=method_params, exec_num=2)
     elif args.task == 'odt':
         alb_aug_odt(args.base_dir, args.split, lbl_format=args.lbl_format, method_params=method_params, exec_num=2)
     elif args.task == 'seg':
