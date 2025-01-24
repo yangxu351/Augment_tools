50d3efb7bf5cd1bc04ecab573f04a4779a8f484b
yangxu351 <1554404832@qq.com>
2025-01-24 16:48:23 +0800

    change params of cls aug

--- aug_preprocess/aug_for_cls_withParam.py
+++ aug_preprocess/aug_for_cls_withParam.py
@@ -6,42 +6,43 @@ from img_albumentation import img_aug_param as albaug
 import albumentations as alb
 import datetime
 
-def alb_aug_cls(base_dir, split='val', method_params={}, combine=False, exec_num=1):
+def alb_aug_cls(base_dir, split=['val'], method_params={}, combine=False, exec_num=1):
     '''
     method_params dict {
                         'rotate': {'limit':(-90,90)}
                         'crop': {'left_px':-10, 'right_px':10, 'top_px':-10, 'bottom_px':10}
                         }
     '''
-
-    source_dir = os.path.join(base_dir, split)
-    save_dir = os.path.join(base_dir , split + '_albaug_param')
-    make_dir_if_not_exist(save_dir, rm=False)
-
     all_trans = albaug.get_trans() 
     dict_trans = {}
     for method, params in method_params.items():
         dict_trans[method] = all_trans[method](**params)
 
-    cat_folders = os.listdir(source_dir)
+    for sp in split:
+        source_data_dir = os.path.join(base_dir, 'inputs', f'{sp}_dataset')
+        # source_dir = os.path.join(base_dir, split)
+        save_dir = os.path.join(base_dir ,  'outputs', f'Alb_aug_{sp}_dataset')
+        make_dir_if_not_exist(save_dir, rm=False)
+
+        cat_folders = os.listdir(source_data_dir)
     
-    for cf in cat_folders:
-        img_names = os.listdir(os.path.join(source_dir, cf))
-        img_names.sort()
-        img_list = []
-        for en in range(exec_num):
-            current_time = datetime.datetime.now()
-            formatted_time = current_time.strftime("%m%d-%H%M")
-            for key, aug in dict_trans.items():
-                c_save_dir = os.path.join(save_dir, cf, key)
-                make_dir_if_not_exist(c_save_dir, rm=True)
-                for n in img_names[:2]:# FIXME:
-                    img = np.array(Image.open(os.path.join(source_dir,cf,n)))
-                    img_name_pref, img_name_suff = n.split('.')
-                    # img_list.append(img)
-                    n_img = aug(image=img)['image']
-                    n_file = os.path.join(c_save_dir, f"{img_name_pref}_ex{en+1}_{formatted_time}.{img_name_suff}")
-                    Image.fromarray(n_img).save(n_file)
+        for cf in cat_folders:
+            img_names = os.listdir(os.path.join(source_data_dir, cf))
+            img_names.sort()
+            img_list = []
+            for en in range(exec_num):
+                current_time = datetime.datetime.now()
+                formatted_time = current_time.strftime("%m%d-%H%M")
+                for key, aug in dict_trans.items():
+                    c_save_dir = os.path.join(save_dir, cf, key)
+                    make_dir_if_not_exist(c_save_dir, rm=True)
+                    for n in img_names:# FIXME:
+                        img = np.array(Image.open(os.path.join(source_data_dir,cf,n)))
+                        img_name_pref, img_name_suff = n.split('.')
+                        # img_list.append(img)
+                        n_img = aug(image=img)['image']
+                        n_file = os.path.join(c_save_dir, f"{img_name_pref}_ex{en+1}_{formatted_time}.{img_name_suff}")
+                        Image.fromarray(n_img).save(n_file)
 
                 # if combine:
                 #     aug = alb.Compose([*dict_trans.values()])
